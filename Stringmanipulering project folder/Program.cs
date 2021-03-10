b59998978916d6fb60a61a0d8c8d670946b5c87c
Leo <leo.anderssonguillou@elev.ga.ntig.se>
2021-03-10 15:00:43 +0100

    Error if custom name has spaces in it

--- Stringmanipulering project folder/Program.cs
+++ Stringmanipulering project folder/Program.cs
@@ -9,7 +9,27 @@ namespace Stringmanipulering_project
             string[] highScores = { "LENA 100000", "MICKE 23004", "MARTIN 2000" };
             string[] person;
             int totalScore = 0;
+            string customName = "";
+            int customScore = 0;
 
+            Console.WriteLine("Skriv ett namn:");
+            customName = Console.ReadLine();
+            customName = customName.Trim();
+            customName = customName.ToUpper();
+
+
+            bool success = false;
+            Console.WriteLine("Skriv hens poäng:");
+            while (!success)
+            {
+                success = int.TryParse(Console.ReadLine(), out customScore);
+                if (success == false)
+                {
+                    Console.WriteLine("Poängen måste vara ett rimligt heltal:");
+                }
+            }
+            highScores[2] = customName + " " + customScore;
+            Console.WriteLine();
 
             for (int i = 0; i < 3; i++)
             {
